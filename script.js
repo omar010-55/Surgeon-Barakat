9ef6a2e26e24447b42734762776b4a43a3e28c69
omar mohamed <32615699+omar010-55@users.noreply.github.com>
2025-04-26 17:27:54 +0300

    Add the berger menu function
    and the css for hover effect and fix google fonts

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 const ul = document.querySelector(".hero header ul")
 const menu =document.querySelector("#menu")
-function showMenu() {
+let isOn = false
 
+function state() {
+  let checkState = () => isOn
+  let changeState = () => isOn ? isOn = false : isOn = true;
+  return {checkState, changeState}
 }
+
+function showMenu() {
+  if(state().checkState() === false) {
+    menu.textContent = "close"
+  } else {
+    menu.textContent = "menu"
+  }
+  ul.classList.toggle("on")
+  state().changeState()
+}
+menu.addEventListener("click", showMenu)
